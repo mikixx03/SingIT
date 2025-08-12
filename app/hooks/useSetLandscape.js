86b9acf76595fb922a83bc41c0a877a63d11b73d
Mikolaj <mkljszarejko@gmail.com>
2025-08-12 21:38:12 +0200

    setting gameScreen to landscape

---
+++ app/hooks/useSetLandscape.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as ScreenOrientation from "expo-screen-orientation";
+
+export default function useSetLandscape() {
+  React.useEffect(() => {
+    async function changeScreenOrientation() {
+      try {
+        await ScreenOrientation.lockAsync(
+          ScreenOrientation.OrientationLock.LANDSCAPE
+        );
+      } catch (error) {
+        console.log("Error occured during landscape screen lock");
+      }
+    }
+    changeScreenOrientation();
+
+    return () => ScreenOrientation.unlockAsync();
+  }, []);
+}
--- app/screens/gameScreen.js
+++ app/screens/gameScreen.js
@@ -1,7 +1,9 @@
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView, Text } from "react-native";
+import useSetLandscape from "../hooks/useSetLandscape";
 
 export default function GameScreen() {
+  useSetLandscape();
   return (
     <SafeAreaView>
       <Text>GameScreen</Text>
